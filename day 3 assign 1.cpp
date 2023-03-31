ff211b67631e3d4ac54bc7c9a2150e82278225a5
192211500 <123438408+harsha123io@users.noreply.github.com>
2023-03-30 19:28:34 -0700

    Add files via upload

---
+++ day 3 assign 1.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void displayPrimeNumbers(int start, int end) {
+    printf("Prime numbers between %d and %d are:\n", start, end);
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+}
+
+int main() {
+    int start, end;
+    printf("Enter the start and end of the interval: ");
+    scanf("%d %d", &start, &end);
+    displayPrimeNumbers(start, end);
+    return 0;
+}
+
